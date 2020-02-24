bb4d4dfd8880bf5e2ee6a5db2976684691359f96
realhet <real_het@hotmail.com>
2020-02-24 01:37:38 +0100

    code editor experimental switch statement parser

--- het/test/testCodeEditor.d
+++ het/test/testCodeEditor.d
@@ -7,6 +7,84 @@
 
 import het, het.ui, het.keywords, het.tokenizer, het.parser;
 
+//todo: commentek hozzacsapasa a tokenekhez. Ha a comment soraban van token, akkor ahhoz, ha nincs akkor meg a kovetkezo tokenhez, kiveve ha a legvegen van. De mi van, ha tobb comment van egymas mellett??
+
+//todo: a sebesseghez a tokenizernek meg az utana kovetkezo lepeseknek is range-knak kell lenniuk.
+
+enum testCase = q{
+import std.stdio, het.utils;
+
+void main() {
+    foreach (value; [ 1, 2, 3, 10, 20 ]) {
+        writefln("--- value: %s ---", value);
+
+        switch (value) {
+
+        case 1:
+            writeln("case 1");
+            goto case;
+
+        case 2:
+            writeln("case 2");
+            goto case 10;
+
+        case 3:
+            writeln("case 3");
+            goto default;
+
+        case 10:
+            writeln("case 10");
+            break;
+
+        case 11: .. case 15:
+            writeln("case 11..15");
+            continue; // continue the outer loop
+
+        case 20: case 21: case 25:
+            writeln("case 20, 21, 25");
+            break;
+
+        default:
+            writeln("default");
+            break;
+        }
+    }
+}
+};
+
+
+
+auto noComments(Token[] t){ return t.filter!(t => !t.isComment).array; }
+
+Token[][] splitToSentences(Token[] tokens){
+  if(tokens.empty) return [];
+
+  auto level = tokens[0].level;
+  auto separators = [-1];
+  auto lastTokenIdx = tokens.length.to!int-1;
+  foreach(int i, ref t; tokens){
+    if(t.level==level && t.kind==TokenKind.Operator && t.id.among(opcolon, opsemiColon, opcurlyBracketClose)){
+
+      //include the last comments in the same line
+      int j = i;
+      while(j<lastTokenIdx && tokens[j+1].isComment && t.line == tokens[j+1].line) j++;
+
+      separators ~= j;
+    }
+  }
+
+  if(separators[$-1] != lastTokenIdx)
+    separators ~= lastTokenIdx;
+
+  Token[][] res;
+  foreach(int i; 0..separators.length.to!int-1)
+    res ~= tokens[separators[i]+1 .. separators[i+1]+1];
+
+  return res;
+}
+
+
+
 class FrmMain: GLWindow { mixin autoCreate; // !FrmMain ////////////////////////////
 
   Parser parser;
@@ -22,27 +100,11 @@ class FrmMain: GLWindow { mixin autoCreate; // !FrmMain ////////////////////////
     parser = new Parser;
 
     auto f = File(`..\Dialogs.d`);
-    parser.tokenize(f.fullName);
+    parser.tokenize(f.fullName, testCase);
 
-    //isolate tokenSentences
-    auto separators = [-1];
-    auto lastTokenIdx = parser.tokens.length.to!int-1;
-    foreach(int i, ref t; parser.tokens){
-      if(t.level==0 && t.kind==TokenKind.Operator && t.id.among(opcolon, opsemiColon, opcurlyBracketClose)){
+    parser.tokens = parser.tokens.noComments; //todo: deal with tokens later
 
-        //attach comment in the same line
-        int j = i;
-        while(j<lastTokenIdx && parser.tokens[j+1].isComment && t.line == parser.tokens[j+1].line) j++;
-
-        separators ~= j;
-      }
-    }
-
-    if(separators[$-1] != lastTokenIdx)
-      separators ~= lastTokenIdx;
-
-    foreach(int i; 0..separators.length.to!int-1)
-      tokenSentences ~= parser.tokens[separators[i]+1 .. separators[i+1]+1];
+    tokenSentences = parser.tokens.splitToSentences; //todo: input ranges
   }
 
   override void onDestroy(){ // destroy //////////////////////////////
@@ -79,43 +141,142 @@ class FrmMain: GLWindow { mixin autoCreate; // !FrmMain ////////////////////////
 
       }*/
 
-
-      foreach(ts; tokenSentences){
-        if(ts[0].source=="import"){
-          Row({
-            //theme = "tool";
-
-            padding = "2";
-            margin = "2";
-            border = "normal";
-            bkColor = clOrange;
-            style.bkColor = bkColor;
-            Comment("static");
-            Text(bold(" import "));
-
-            string s0;
-            //Row(s0, {
-              string name;
+      void header(RGB color, string title){ with(im){
+        theme = "tool";
+        padding = "2 2 2 2";
+        margin = "2";
+        border = "1 normal";
+        //border.extendBottomRight = true;
+        bkColor = lerp(color, clWhite, .5);
+        border.color = lerp(bkColor, clBlack, .25);
+        style.bkColor = bkColor;
+        if(title.length) Text(bold(title~" "));
+      }}
 
 
-              foreach(ref t; ts[1..$]){
-                if(t.kind==TokenKind.Identifier || t.source.among(".", "=", ":")) name ~= t.source;
-                if(t.source.among(",", ":")) { Edit(name, { bkColor = style.bkColor; }); Text(", "); name = ""; }
+      void uiTokenSentences(Token[][] sentences){
+
+        foreach(ts; sentences){
+          if(ts[0].source=="import"){
+            Row({
+              header(clRainbowYellow, "import");
+              Row({
+                string name;
+
+                foreach(ref t; ts[1..$]){
+                  if(t.kind==TokenKind.Identifier || t.source.among(".", "=", ":")) name ~= t.source;
+                  if(t.source.among(",", ":")) { Edit(name, { bkColor = style.bkColor; }); name = ""; }
+                }
+
+                if(!name.empty){
+                  Edit(name, { bkColor = style.bkColor; }); name = "";
+                }
+              });
+            });
+
+          }else if(ts[$-1].source=="}"){ //block thing
+
+            //search the start of the block
+            auto level = ts[0].level; //the same as ts[$-1].level
+            auto blockStart = ts.retro.countUntil!(t => t.source=="{" && t.level==level);
+            assert(blockStart>=0);
+            blockStart = ts.length-1-blockStart;
+
+            Column({
+              header(clSkyBlue, "");
+              Row({
+                foreach(t; ts[0..blockStart]){ Text(t.source, " "); }
+              });
+
+
+              if(ts[0].source=="switch"){
+                Column({
+                  header(clWhite, "");
+                  auto sentences = ts[blockStart+1 .. $-1].splitToSentences;
+
+                  //here gotta split the cases and the sentences
+
+                  while(sentences.length){
+                    string[] cases;
+                    bool isRange;
+
+                    while(sentences.length && sentences[0][$-1].source==":"){
+                      auto cs = sentences[0][0..$-1]; //strip off :
+                      sentences = sentences[1..$];
+
+                      enforce(!cs.empty);
+
+                      if(cs[0].source=="default"){
+                        cases = ["default"];
+                      }else{
+                        if(cs[0].source==".."){
+                          enforce(!isRange && cases.length==1);
+                          cs = cs[1..$];
+                          isRange = true;
+                        }
+
+                        enforce(cs.length && cs[0].source=="case");
+                        cs = cs[1..$];
+
+                        enforce(cs.length);
+
+                        cases ~= cs.map!"a.source".join(" ");
+                      }
+                    }
+
+                    auto s = cases.join(isRange ? " .. " : ", ");
+                    Row({
+                      Text(s, "  \t");
+                      Bullet;
+
+                      border = "normal";
+                      border.extendBottomRight = true;
+                      border.color = clSilver;
+                      flags.yAlign = YAlign.top;
+
+
+                      Token[][] temp;
+                      while(sentences.length && sentences[0][$-1].source != ":"){
+                        temp ~= sentences[0];
+                        sentences = sentences[1..$];
+                      }
+
+                      Column({ //border = "normal";
+                        uiTokenSentences(temp); //recursive
+                      });
+                    });
+                  }
+                });
+              }else{
+                Column({
+                  header(clWhite, "");
+                  auto sent = ts[blockStart+1 .. $-1].splitToSentences;
+                  uiTokenSentences(sent); //recursive
+                });
               }
-            //});
-          });
 
-        }else{
-          Column({
-            border = "normal";
-            foreach(ref t; ts){
-              Row({ Text("%t".format(t), "\n"); });
-            }
-          });
+            });
+          }else if(ts[$-1].source.among(":", ";")){ //statement
+            Row({
+              //header(clWhite, "");
+              Text(ts.map!(a => a.source).join(" "));
+            });
+          }else{
+            Column({
+              border = "normal";
+              foreach(ref t; ts){
+                Row({ Text("%t".format(t), "\n"); });
+              }
+            });
+          }
         }
+
       }
 
-    });
+      uiTokenSentences(tokenSentences);
+
+    }); //panel
+
 
   }
 
